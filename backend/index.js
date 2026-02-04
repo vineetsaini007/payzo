512a946704b34fbbfdb6bc99a287f9889639e7f1
vineetsaini007 <vineetsainiii007@gmail.com>
2026-02-04 16:50:24 +0530

    defining router structure

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,11 @@
 const express = require("express");
+const rootRouter = require("./Routes/index");
+const cors = require("cors");
 
+app.use(cors());
+app.use(express.json());
+
+const app = express();
+
+app.use("/api/v1", rootRouter)
 
