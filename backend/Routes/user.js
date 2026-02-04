34920d0ee5634c3bb09ca0cfeb3d12ae7eb1215d
vineetsaini007 <vineetsainiii007@gmail.com>
2026-02-04 21:53:51 +0530

    adding auth middleware

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -6,6 +6,7 @@ const zod = require("zod");
 const { User } = require("../db");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
+const  { authMiddleware } = require("../middleware");
 
 const signupBody = zod.object({
     username: zod.string().email(),
@@ -82,6 +83,29 @@ router.post("/signin", async (req, res) => {
 
     res.status(411).json({
         message: "Error while logging in"
+    })
+})
+
+const updateBody = zod.object({
+	password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional(),
+})
+
+router.put("/", authMiddleware, async (req, res) => {
+    const { success } = updateBody.safeParse(req.body)
+    if (!success) {
+        res.status(411).json({
+            message: "Error while updating information"
+        })
+    }
+
+    await User.updateOne(req.body, {
+        _id: req.userId
+    })
+
+    res.json({
+        message: "Updated successfully"
     })
 })
 
