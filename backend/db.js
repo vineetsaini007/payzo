f5281bf98bf967553030a523b423d9fed7f8cb19
vineetsaini007 <vineetsainiii007@gmail.com>
2026-02-11 22:05:16 +0530

    bug fix

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('process.env.MONGO_URL')
+mongoose.connect('mongodb+srv://vineetsainiii007_db_user:uLrsg8Z07FbmCnnU@vineet.tetyk5v.mongodb.net/paytm')
 .then(() => {
     console.log("database connected");
 })
